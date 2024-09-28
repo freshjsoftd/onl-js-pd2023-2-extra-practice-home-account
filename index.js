dd6eae5f828e292f57191f86da8f977beead2228
yan-freshcode <freshjs.oftd@gmail.com>
2024-09-28 13:23:21 +0300

    Add models and migration without associations

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const http = require('http')
 require('dotenv').config()
 
 const app = require('./src/app')
+const db = require('./src/db/models')
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+const dbCheck = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log(`Connection to DB ${process.env.DB_NAME} has been done`)
+  } catch (error) {
+    console.log(`Can't connect to DB ${process.env.DB_NAME}`, error.message)
+  }
+}
+
+dbCheck()
 
 server.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
---
+++ src/db/models/shop.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class shop extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  shop.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    description: DataTypes.TEXT,
+    url: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'shop',
+  });
+  return shop;
+};
