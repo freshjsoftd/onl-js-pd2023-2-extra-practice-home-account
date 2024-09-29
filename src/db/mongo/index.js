b929a92d7d7640575104b068799f8f809fc08fbf
yan-freshcode <freshjs.oftd@gmail.com>
2024-09-29 13:05:55 +0300

    Add mongoDB with models

---
+++ src/db/mongo/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+// ==================================
+const mongoose = require('mongoose');
+// ==================================
+const env = process.env.NODE_ENV || 'development';
+const basename = path.basename(__filename);
+const configPath = path.resolve('src', 'config', 'mongoConfig');
+
+const config = require(configPath)[env];
+
+mongoose
+	.connect(`mongodb://${config.host}:${config.port}/${config.dbName}`)
+	.then(() => console.log(`Connect to DB <<< ${config.dbName} >>> is done`))
+	.catch((err) => console.log(err));
+
+const dbMongo = {};
+
+fs.readdirSync(__dirname)
+	.filter((file) => {
+		return (
+			file.indexOf('.') !== 0 &&
+			file !== basename &&
+			file.slice(-3) === '.js' &&
+			file.indexOf('.test.js') === -1
+		);
+	})
+	.forEach((file) => {
+		const model = require(path.resolve(__dirname, file))
+		dbMongo[model.modelName] = model;//Role: Role.js
+	});
+
+  dbMongo.mongoose = mongoose;
+
+  module.exports = dbMongo;
